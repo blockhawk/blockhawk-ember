bb70033c02d65336e999ea5dad3cd3ca2df6b6e6
Gianni Settino <g@miltonandparc.com>
2017-09-22 01:18:40 -0400

    Demo route now waits for web3 init

--- app/routes/demo.js
+++ app/routes/demo.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
+const { RSVP } = Ember;
+
 export default Ember.Route.extend({
+  web3: Ember.inject.service(),
+
+  beforeModel() {
+    return new RSVP.Promise(resolve => {
+      window.addEventListener('load', () => {
+        // Force web3 init
+        this.get('web3');
+        resolve();
+      });
+    });
+  }
 });
--- tests/unit/routes/demo-test.js
+++ tests/unit/routes/demo-test.js
@@ -1,8 +1,7 @@
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:demo', 'Unit | Route | demo', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['service:web3']
 });
 
 test('it exists', function(assert) {
