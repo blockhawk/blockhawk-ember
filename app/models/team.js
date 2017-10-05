780cbb5de907abfc9e1395ea00e8850af6bca2c3
Gianni Settino <g@miltonandparc.com>
2017-10-05 02:38:02 -0400

    Adds team images

--- app/models/team.js
+++ app/models/team.js
@@ -1,6 +1,15 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+
+const { computed } = Ember;
 
 export default DS.Model.extend({
   city: DS.attr('string'),
-  nickname: DS.attr('string')
+  nickname: DS.attr('string'),
+
+  imageUrl: computed('city', 'nickname', function() {
+    const city = this.get('city').replace(' ', '');
+    const nickname = this.get('nickname');
+    return `https://static-hosted.stats.com/GZ/images/NFLlogos/${city}${nickname}.png`;
+  })
 });
